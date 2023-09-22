ab8af3102a0103a4b137fa4c2cf96345ce1acd9b
Arash Anbari <Arash.Anbari@yh.nackademin.se>
2023-09-22 15:12:17 +0200

    22-09 - Added gallery room and teleport

---
+++ APK/ESCTest+Room1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/ShapesXr_CodeGen.c
@@ -0,0 +1,45 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ShapesXr.SpaceDescriptor::.ctor()
+extern void SpaceDescriptor__ctor_m03276C02E1173FBDB56435C622661E82AAB9E004 (void);
+static Il2CppMethodPointer s_methodPointers[1] = 
+{
+	SpaceDescriptor__ctor_m03276C02E1173FBDB56435C622661E82AAB9E004,
+};
+static const int32_t s_InvokerIndices[1] = 
+{
+	6795,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_ShapesXr_CodeGenModule;
+const Il2CppCodeGenModule g_ShapesXr_CodeGenModule = 
+{
+	"ShapesXr.dll",
+	1,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
